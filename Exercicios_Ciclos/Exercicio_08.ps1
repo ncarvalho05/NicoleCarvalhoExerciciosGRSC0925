3ae56d4f081af7e272277db280608e56260c4e1b
ncarvalho05 <Nicole.Carvalho.T0132027@edu.atec.pt>
2026-03-02 15:19:56 +0000

    series output

---
+++ Exercicios_Ciclos/Exercicio_08.ps1
@@ -0,0 +1,5 @@
+# Série 1
+for ($i = 10; $i -le 1000; $i += 10) { Write-Host $i -NoNewline; Write-Host " " -NoNewline }
+Write-Host "`n---"
+# Série 2
+for ($i = 15; $i -le 995; $i += 10) { Write-Host $i -NoNewline; Write-Host " " -NoNewline }
